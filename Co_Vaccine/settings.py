305d447d74b5b23635dd0527e2e55e3350a247f3
Kush Vora <vorakush@gmail.com>
2021-09-01 10:57:14 +0530

    add password to env

--- Co_Vaccine/settings.py
+++ Co_Vaccine/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import dj_database_url
 
@@ -36,7 +42,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kz4%s^8p(^wn4=bo(&j$ls-q%q^a0o4$3)0*#y(2ll+hb5)*h1'
+SECRET_KEY = env('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -173,11 +179,15 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
 
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'covaccine.gov@gmail.com'
-EMAIL_HOST_PASSWORD = 'covaccine_kdm'
+EMAIL_HOST_USER = env('email_user')
+EMAIL_HOST_PASSWORD = env('email_pass')
 EMAIL_USE_TLS = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
